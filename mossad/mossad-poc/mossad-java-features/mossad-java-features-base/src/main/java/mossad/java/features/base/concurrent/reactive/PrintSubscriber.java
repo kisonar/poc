a7f90917acdc07db5ef654d73ad660a8665bd114
Marcin Migdal <marcin.migdal@nokia.com>
2018-06-08 14:43:04 +0200

    JAVA 9 features

---
+++ mossad/mossad-poc/mossad-java-features/mossad-java-features-base/src/main/java/mossad/java/features/base/concurrent/reactive/PrintSubscriber.java
@@ -0,0 +1,32 @@
+package mossad.java.features.base.concurrent.reactive;
+
+import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.Flow.Subscriber;
+
+public class PrintSubscriber implements Subscriber<Integer> {
+
+    private Subscription subscription;
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1);
+    }
+
+    @Override
+    public void onNext(Integer item) {
+        System.out.println("Received item: " + item);
+        subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable error) {
+        System.out.println("Error occurred: " + error.getMessage());
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("PrintSubscriber is complete");
+    }
+
+}
---
+++ mossad/mossad-poc/mossad-java-features/mossad-java-features-client/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module mossad.java.features.client {
+    requires mossad.java.features.services;
+    requires mossad.java.features.library;
+}
