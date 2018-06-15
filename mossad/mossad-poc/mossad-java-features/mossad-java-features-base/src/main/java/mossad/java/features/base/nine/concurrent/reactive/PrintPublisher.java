8d4e07c47ebffa7416b90158d89242850a734167
Marcin Migdal <marcin.migdal@nokia.com>
2018-06-15 16:19:19 +0200

    Preparing to reactive programming model
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

---
+++ mossad/mossad-poc/mossad-java-features/mossad-java-features-base/src/main/java/mossad/java/features/base/nine/concurrent/reactive/PrintPublisher.java
@@ -0,0 +1,12 @@
+package mossad.java.features.base.nine.concurrent.reactive;
+
+import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Publisher;
+
+public class PrintPublisher implements Publisher<Integer> {
+
+    @Override
+    public void subscribe(Flow.Subscriber subscriber) {
+
+    }
+}
