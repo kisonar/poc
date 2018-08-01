037dbc57e4b3d59e4ca30ed1ab2a0b4209694fc4
Marcin Migdal <marcin.migdal@nokia.com>
2018-08-01 14:59:23 +0200

    Adding SpringBoot related classes
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

---
+++ mossad/mossad-poc/mossad-spring/mossad-spring-web/src/main/java/com/mossad/mmigdal/spring/web/Main.java
@@ -0,0 +1,15 @@
+package com.mossad.mmigdal.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
