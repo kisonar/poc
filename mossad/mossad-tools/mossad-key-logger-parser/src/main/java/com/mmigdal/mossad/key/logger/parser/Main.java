abb49cfb776669a9195b1959f281ac912d21a063
Marcin Migdal <marcinmigdal@wp.pl>
2018-08-07 23:42:14 +0200

    Adding base for UI

---
+++ mossad/mossad-tools/mossad-key-logger-parser/src/main/java/com/mmigdal/mossad/key/logger/parser/Main.java
@@ -0,0 +1,21 @@
+package com.mmigdal.mossad.key.logger.parser;
+
+import com.mmigdal.mossad.key.logger.parser.ui.ParserPanel;
+
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main (String [] args){
+
+
+        JFrame applicationFrame = new JFrame();
+        applicationFrame.add(new ParserPanel());
+
+
+
+
+    }
+
+}
---
+++ mossad/mossad-tools/mossad-key-logger-parser/src/main/java/com/mmigdal/mossad/key/logger/parser/ui/ParserPanel.java
@@ -0,0 +1,11 @@
+package com.mmigdal.mossad.key.logger.parser.ui;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class ParserPanel extends JPanel {
+
+    public ParserPanel(){
+        this.setLayout(new GridLayout());
+    }
+}
