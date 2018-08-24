fe6238928c64bc481153545f01a3f9f4592689cc
Marcin Migdal <marcin.migdal@nokia.com>
2018-08-24 15:22:56 +0200

    Adding Junit against StreamsImprovement
    //TODO
    filtering per words
    Junit
    Multithreading
    Measurements
    
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

---
+++ mossad/mossad-poc/mossad-java-features/mossad-java-features-base/src/test/java/mossad.java.features.base.nine.streams/StreamsImprovementTest.java
@@ -0,0 +1,12 @@
+package mossad.java.features.base.nine.streams;
+
+import org.junit.jupiter.api.Test;
+
+public class StreamsImprovementTest {
+
+    @Test
+    public void usageCheckTest() {
+        StreamsImprovement.usageCheck();
+    }
+
+}
