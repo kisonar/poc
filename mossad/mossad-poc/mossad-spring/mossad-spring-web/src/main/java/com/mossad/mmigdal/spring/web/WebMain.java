f9f37d3f4181f43703ebab4d27d334ab8a1314bd
Marcin Migdal <marcin.migdal@nokia.com>
2018-08-27 16:37:54 +0200

    adding spring SFTP
    //TODO
    filtering per words
    Junit
    Multithreading
    Measurements
    
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- mossad/mossad-poc/mossad-spring/mossad-spring-web/src/main/java/com/mossad/mmigdal/spring/web/WebMain.java
+++ mossad/mossad-poc/mossad-spring/mossad-spring-web/src/main/java/com/mossad/mmigdal/spring/web/WebMain.java
@@ -1,15 +1,17 @@
 package com.mossad.mmigdal.spring.web;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Main {
+public class WebMain {
+
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(WebMain.class, args);
     }
 
 }
