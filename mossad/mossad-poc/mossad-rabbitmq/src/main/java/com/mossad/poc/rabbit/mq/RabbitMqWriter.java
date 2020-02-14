7c3edc15d7f06040fecdb1093cd1503fc5a78e4a
Marcin Migdal <marcin.migdal@nokia.com>
2020-02-14 16:40:39 +0100

    Works Gerry mode
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- mossad/mossad-poc/mossad-rabbitmq/src/main/java/com/mossad/poc/rabbit/mq/RabbitMqWriter.java
+++ mossad/mossad-poc/mossad-rabbitmq/src/main/java/com/mossad/poc/rabbit/mq/RabbitMqWriter.java
@@ -3,22 +3,27 @@ package com.mossad.poc.rabbit.mq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class RabbitMqWriter {
 
-    public static void main(String[] args) {
+    //static String QUEUE_NAME = "svc-test-2";
+    static String QUEUE_NAME = "svcc_dena_dfdr11_filer_stats_q";
 
+    public static void main(String[] args) {
         /*
         String userName = "guest";
         String password = "guest";
-        String virtualHost = "/";
+        String virtualHost = "virtual-host";
+        int portNumber = 9200;
         String hostName = "10.133.131.152";
-        int portNumber = 9001;
         */
-        String userName = "vcc_DENA";
-        String password = "DEN@2020";
+        //String hostName = "localhost";
+
+        String userName = "svcc_DENA";
+        String password = "DEN@2020!";
         String virtualHost = "svcc";
         String hostName = "135.3.65.36";
         int portNumber = 5672;
@@ -28,31 +33,21 @@ public class RabbitMqWriter {
         factory.setVirtualHost(virtualHost);
         factory.setHost(hostName);
         factory.setPort(portNumber);
-
-        Channel channel = null;
-        Connection conn = null;
         System.out.println("Starting");
-        try {
-            conn = factory.newConnection();
-            System.out.println("connection");
-            channel = conn.createChannel();
-            System.out.println("channel");
-            System.out.println("Closign channel");
-            System.out.println("Closing connection");
+        try (
+                Connection conn = factory.newConnection();
+                Channel channel = conn.createChannel()
+        ) {
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (TimeoutException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                channel.close();
-                conn.close();
-            } catch (IOException e) {
-
-            } catch (TimeoutException e) {
-                e.printStackTrace();
-            }
-
         }
 
 
