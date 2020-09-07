50faa0e941f43927341097882d0682a0a40cc35c
Marcin Migdal <marcin.migdal@nokia.com>
2020-09-07 13:42:06 +0200

    Apply changes

--- mossad/mossad-tools/mossad-key-logger-parser/src/test/java/com/mmigdal/mossad/key/logger/parser/logic/LogicFlowTest.java
+++ mossad/mossad-tools/mossad-key-logger-parser/src/test/java/com/mmigdal/mossad/key/logger/parser/logic/LogicFlowTest.java
@@ -2,21 +2,20 @@ package com.mmigdal.mossad.key.logger.parser.logic;
 
 import java.util.Calendar;
 import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
-
+import java.util.List;
 
 public class LogicFlowTest {
 
-    private final static String INPUT_2020 = "2020";
-    private final static String OUTPUT_2000 = "2020";
+    private final static List<String> years = List.of("2018","2019","2020");
     private LogicFlow logicFlow;
     private long startTime;
     private long endTime;
-    private String inputPath;
-    private String outputPath;
+    private String yearPath;
 
     @BeforeEach
     public void setUpBeforeEach() {
@@ -33,28 +32,29 @@ public class LogicFlowTest {
     @Test
     public void processFile_whenLinux_2020() {
         String generalLinuxPath = "/run/media/marcin/MigiBigDisk/repozytoria/repo-arbeit-client/trunk/korpo/toBeDestroyed/tools/wlam/input/";
-        inputPath = generalLinuxPath + INPUT_2020;
-        outputPath = generalLinuxPath + OUTPUT_2000;
-        logicFlow = new LogicFlow(
-            inputPath,
-            outputPath);
-        logicFlow.execute();
+        for ( String year: years) {
+            yearPath = generalLinuxPath + year;
+            logicFlow = new LogicFlow(
+                yearPath,
+                yearPath);
+            logicFlow.execute();
+        }
     }
 
     @EnabledOnOs(OS.WINDOWS)
     @Test
     public void processFile_whenWindows_2020() {
         String generalWindowsPath = "G:\\repozytoria\\repo_test\\korpo\\toBeDestroyed\\tools\\wlam\\output\\";
-        inputPath = generalWindowsPath + INPUT_2020;
-        outputPath = generalWindowsPath + OUTPUT_2000;
-        logicFlow = new LogicFlow(
-            inputPath,
-            outputPath);
-        logicFlow.execute();
+        for ( String year: years) {
+            yearPath = generalWindowsPath + year;
+            logicFlow = new LogicFlow(
+                yearPath,
+                yearPath);
+            logicFlow.execute();
+        }
     }
 
     private long getTime() {
-
         return Calendar.getInstance().getTime().getTime();
         //return LocalDateTime.now().getSecond();
     }
