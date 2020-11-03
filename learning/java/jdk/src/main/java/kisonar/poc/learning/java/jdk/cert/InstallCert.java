36a2530e398206c5898e813e37210a0cd2cc6f68
Marcin Migdal <marcin.migdal@nokia.com>
2020-11-03 11:38:35 +0100

    Reorganization part 8

--- learning/java/jdk/src/main/java/kisonar/poc/learning/java/jdk/cert/InstallCert.java
+++ learning/java/jdk/src/main/java/kisonar/poc/learning/java/jdk/cert/InstallCert.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mossad.poc.others.ocjp.certs;
+package kisonar.poc.learning.java.jdk.cert;
 
 /**
  * @author mmigdal
