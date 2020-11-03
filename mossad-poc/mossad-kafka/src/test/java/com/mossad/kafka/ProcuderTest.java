c9d17d5c71a557c65614cac81dd5c70d8386e7e4
Marcin Migdal <marcin.migdal@nokia.com>
2020-11-03 11:08:18 +0100

    Reorganization part 5

--- mossad-poc/mossad-kafka/src/test/java/com/mossad/kafka/ProcuderTest.java
+++
@@ -1,5 +0,0 @@
-package com.mossad.kafka;
-
-public class ProcuderTest {
-
-}
---
+++ services/kafka/src/main/java/kisonar/poc/services/kafka/KafkaTopicNames.java
@@ -0,0 +1,11 @@
+package kisonar.poc.services.kafka;
+
+public final class KafkaTopicNames {
+
+    public static final String TOPIC_READER = "test";
+    public static final String TOPIC_WRITER = "test";
+
+    private KafkaTopicNames() {
+
+    }
+}
