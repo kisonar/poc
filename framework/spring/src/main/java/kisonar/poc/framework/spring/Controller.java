4387670198a31e962f3da9f0e319286c69d64f41
kisonar <kisonar@wp.pl>
2021-07-04 12:23:27 +0200

    Fix basic Spring with Gradle setup

--- framework/spring/src/main/java/kisonar/poc/framework/spring/Controller.java
+++ framework/spring/src/main/java/kisonar/poc/framework/spring/Controller.java
@@ -1,8 +1,8 @@
 package kisonar.poc.framework.spring;
 
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
+//import org.apache.log4j.Logger;
+//import org.apache.log4j.LogManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-  private static final Logger LOG = LogManager.getLogger(Controller.class);
+  //private static final Logger LOG = LogManager.getLogger(Controller.class);
 
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    LOG.info("Invoking endpoint /hello");
+    //LOG.info("Invoking endpoint /hello");
     return String.format("Hello %s!", name);
   }
 
