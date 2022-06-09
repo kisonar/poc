842978dcd9bc22c08a6972467d21b9919a895e6d
Marcin <kisonar@wp.pl>
2022-06-09 08:24:43 +0200

    Test containers

--- framework/spring/src/test/java/kisonar/poc/framework/spring/integration/IntegrationTest.java
+++ framework/spring/src/test/java/kisonar/poc/framework/spring/integration/IntegrationTest.java
@@ -1,35 +1,11 @@
 package kisonar.poc.framework.spring.integration;
 
-import org.junit.ClassRule;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.DockerComposeContainer;
 
-import java.io.File;
-
-public class IntegrationTest {
-
-      @ClassRule
-      public static DockerComposeContainer environment =
-              new DockerComposeContainer(new File("docker-compose.yml"))
-                      .withExposedService("kafka", 9092);
-
-      @BeforeAll
-      public static void setUp() {
-            environment.start();
-      }
-
-      @AfterAll
-      public static void afterAll() {
-            environment.stop();
-            environment.close();
-      }
+public class IntegrationTest extends IntegrationPrepare {
 
       @Test
       public void simpleTest() {
 
-
       }
-
 }
