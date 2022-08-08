294536f10bf53212178110688d9db39f42027706
kisonar <kisonar@wp.pl>
2022-08-08 13:18:02 +0200

    Add wiremock

---
+++ learning/java/jdk/src/test/java/kisonar/poc/learning/java/jdk/collections/EntityHashOkEqualsOkTest.java
@@ -0,0 +1,110 @@
+package kisonar.poc.learning.java.jdk.collections;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static kisonar.poc.learning.java.jdk.collections.CollectionsFactory.getEntityOK;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EntityHashOkEqualsOkTest {
+
+      private final Map<Entity, String> map = new HashMap<>();
+
+      private final String name1 = "one";
+      private final String name2 = "two";
+      private final String name3 = "three";
+      private final String name4 = "four";
+      private final String name5 = "five";
+      private final String value1 = "value1";
+      private final String value2 = "value2";
+      private final String value3 = "value3";
+      private final String value4 = "value4";
+      private final String value5 = "value5";
+      private final int id1 = 1;
+      private final int id2 = 2;
+      private final int id3 = 3;
+      private final int id4 = 4;
+      private final int id5 = 5;
+
+      @BeforeEach
+      public void setUp() {
+            map.clear();
+      }
+
+      @Test
+      public void hashOkEqualsOkWithDifferentKeys() {
+            Entity entityKey1 = getEntityOK(id1, name1, value1);
+            Entity entityKey2 = getEntityOK(id2, name2, value2);
+            Entity entityKey3 = getEntityOK(id3, name3, value3);
+            Entity entityKey4 = getEntityOK(id4, name4, value4);
+            Entity entityKey5 = getEntityOK(id5, name5, value5);
+
+            map.put(entityKey1, value1);
+            map.put(entityKey2, value2);
+            map.put(entityKey3, value3);
+            map.put(entityKey4, value4);
+            map.put(entityKey5, value5);
+
+            assertEquals(5, map.size());
+
+            assertTrue(map.containsKey(entityKey1));
+            assertTrue(map.containsKey(entityKey2));
+            assertTrue(map.containsKey(entityKey3));
+            assertTrue(map.containsKey(entityKey4));
+            assertTrue(map.containsKey(entityKey5));
+            assertTrue(map.containsValue(value1));
+            assertTrue(map.containsValue(value2));
+            assertTrue(map.containsValue(value3));
+            assertTrue(map.containsValue(value4));
+            assertTrue(map.containsValue(value5));
+
+            assertSame(value1, map.get(entityKey1));
+            assertSame(value2, map.get(entityKey2));
+            assertSame(value3, map.get(entityKey3));
+            assertSame(value4, map.get(entityKey4));
+            assertSame(value5, map.get(entityKey5));
+      }
+
+      @Test
+      public void hashOkEqualsOkWithSameKeys() {
+            Entity entityKey1 = getEntityOK(id1, name1, value1);
+            Entity entityKey2 = getEntityOK(id2, name2, value2);
+            Entity entityKey3 = getEntityOK(id3, name3, value3);
+            Entity entityKey4 = getEntityOK(id3, name3, value3); // same as key three
+            Entity entityKey5 = getEntityOK(id3, name3, value5);
+
+            map.put(entityKey1, value1);
+            map.put(entityKey2, value2);
+            map.put(entityKey3, value3);
+            map.put(entityKey4, value4);
+            map.put(entityKey5, value5);
+
+            assertEquals(4, map.size());
+
+            assertTrue(map.containsKey(entityKey1));
+            assertTrue(map.containsKey(entityKey2));
+            assertTrue(map.containsKey(entityKey3));
+            assertTrue(map.containsKey(entityKey4));
+            assertTrue(map.containsKey(entityKey5));
+
+            assertTrue(map.containsValue(value1));
+            assertTrue(map.containsValue(value2));
+            assertFalse(map.containsValue(value3)); //overridden by 4
+            assertTrue(map.containsValue(value4));
+            assertTrue(map.containsValue(value5));
+
+            assertSame(value1, map.get(entityKey1));
+            assertSame(value2, map.get(entityKey2));
+            assertSame(value4, map.get(entityKey3)); //overridden by  4
+            assertSame(value4, map.get(entityKey4));
+            assertSame(value5, map.get(entityKey5));
+
+      }
+
+}
