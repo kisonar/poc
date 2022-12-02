838e98bb686b126bf2a989dcfee095dbc9b5ea18
kisonar <kisonar@wp.pl>
2022-12-02 22:42:48 +0100

    cleanup

--- framework/spring-security/src/main/java/kisonar/poc/framework/spring/security/SpringBootSecurityApp.java
+++ framework/spring-security/src/main/java/kisonar/poc/framework/spring/security/SpringBootSecurityApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class SpringBootSecurityApp {
 
       public static void main(String[] args) {
-            SpringApplication.run(App.class, args);
+            SpringApplication.run(SpringBootSecurityApp.class, args);
       }
 }
