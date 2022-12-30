acf84269f6a8579318664f64d80d7b6f8c68bbf3
Marcin Migdal <marcinmigdal@wp.pl>
2022-12-30 17:28:18 +0100

    test

--- tools/key-logger-parser/src/test/java/com/mmigdal/mossad/key/logger/parser/logic/others/TestingConfiguration.java
+++ tools/key-logger-parser/src/test/java/com/mmigdal/mossad/key/logger/parser/logic/others/TestingConfiguration.java
@@ -10,7 +10,7 @@ public final class TestingConfiguration {
     public final static String GENERAL_PATH_LINUX = "/run/media/marcin/MigiBigDisk/repozytoria/repo-arbeit-client/trunk/korpo/toBeDestroyed/tools/wlam";
     public final static String GENERAL_PATH_WIN = "F:\\repozytoria\\repo-arbeit-client\\korpo\\toBeDestroyed\\tools\\wlam";
     public final static String LINUX_PATH_FQ_INPUT = GENERAL_PATH_LINUX + File.separatorChar + INPUT;
-    public final static String LINUX_PATH_FQ_OUTPUT = GENERAL_PATH_LINUX + File.separatorChar + OUTPUT;
+    public final static String LINUX_PATH_FQ_OUTPUT = GENERAL_PATH_LINUX + File.pathSeparator + OUTPUT;
     public final static String WIN_PATH_FQ_INPUT = GENERAL_PATH_WIN + "\\" + INPUT;
     public final static String WIN_PATH_FQ_OUTPUT = GENERAL_PATH_WIN + "\\" + OUTPUT;
 
