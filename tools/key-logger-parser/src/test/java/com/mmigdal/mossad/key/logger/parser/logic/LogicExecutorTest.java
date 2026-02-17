63604bb81ef34457b2fa71ca3f348c8de8ba90a2
marcin <marcin.migdal@fortum.com>
2026-02-17 16:34:17 +0100

    web-flux

--- tools/key-logger-parser/src/test/java/com/mmigdal/mossad/key/logger/parser/logic/LogicExecutorTest.java
+++ tools/key-logger-parser/src/test/java/com/mmigdal/mossad/key/logger/parser/logic/LogicExecutorTest.java
@@ -6,6 +6,7 @@ import com.mmigdal.mossad.key.logger.parser.logic.model.mode.ModeExecution;
 import com.mmigdal.mossad.key.logger.parser.logic.model.mode.ModeRuntime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -19,56 +20,54 @@ import static com.mmigdal.mossad.key.logger.parser.logic.others.TestingConfigura
 import static com.mmigdal.mossad.key.logger.parser.logic.others.TestingConfiguration.WIN_PATH_FQ_INPUT;
 import static com.mmigdal.mossad.key.logger.parser.logic.others.TestingConfiguration.WIN_PATH_FQ_OUTPUT;
 
+@Disabled
 public class LogicExecutorTest {
 
-    private static Logger LOG = Logger.getLogger(LogicExecutorTest.class.getName());
+      private static final Logger LOG = Logger.getLogger(LogicExecutorTest.class.getName());
+      private long startTime;
 
-    private Logic logic;
-    private long startTime;
-    private long endTime;
+      @BeforeEach
+      public void recordStartTime() {
+            startTime = getTime();
+      }
 
-    @BeforeEach
-    public void recordStartTime() {
-        startTime = getTime();
-    }
+      @AfterEach
+      public void recordEndTime() {
+            long endTime = getTime();
+            LOG.info(String.format("Execution took: %d ms", (endTime - startTime)));
+      }
 
-    @AfterEach
-    public void recordEndTime() {
-        endTime = getTime();
-        LOG.info(String.format("Execution took: %d ms", (endTime - startTime)));
-    }
+      @EnabledOnOs(OS.LINUX)
+      @Test
+      public void processFile_whenLinux_executor_parallel() {
+            executeGenericTest(LINUX_PATH_FQ_INPUT, LINUX_PATH_FQ_OUTPUT, ModeExecution.PARALLEL_FIXED);
+      }
 
-    @EnabledOnOs(OS.LINUX)
-    @Test
-    public void processFile_whenLinux_executor_parallel() {
-        executeGenericTest(LINUX_PATH_FQ_INPUT, LINUX_PATH_FQ_OUTPUT, ModeRuntime.EXECUTOR, ModeExecution.PARALLEL_FIXED);
-    }
+      @EnabledOnOs(OS.LINUX)
+      @Test
+      public void processFile_whenLinux_executor_single() {
+            executeGenericTest(LINUX_PATH_FQ_INPUT, LINUX_PATH_FQ_OUTPUT, ModeExecution.SINGLE);
+      }
 
-    @EnabledOnOs(OS.LINUX)
-    @Test
-    public void processFile_whenLinux_executor_single() {
-        executeGenericTest(LINUX_PATH_FQ_INPUT, LINUX_PATH_FQ_OUTPUT, ModeRuntime.EXECUTOR, ModeExecution.SINGLE);
-    }
+      @EnabledOnOs(OS.WINDOWS)
+      @Test
+      public void processFile_whenWindows_executor_single() {
+            executeGenericTest(WIN_PATH_FQ_INPUT, WIN_PATH_FQ_OUTPUT, ModeExecution.SINGLE);
+      }
 
-    @EnabledOnOs(OS.WINDOWS)
-    @Test
-    public void processFile_whenWindows_executor_single() {
-        executeGenericTest(WIN_PATH_FQ_INPUT, WIN_PATH_FQ_OUTPUT, ModeRuntime.EXECUTOR, ModeExecution.SINGLE);
-    }
+      @EnabledOnOs(OS.WINDOWS)
+      @Test
+      public void processFile_whenWindows_executor_parallel() {
+            executeGenericTest(WIN_PATH_FQ_INPUT, WIN_PATH_FQ_OUTPUT, ModeExecution.PARALLEL_FIXED);
+      }
 
-    @EnabledOnOs(OS.WINDOWS)
-    @Test
-    public void processFile_whenWindows_executor_parallel() {
-        executeGenericTest(WIN_PATH_FQ_INPUT, WIN_PATH_FQ_OUTPUT, ModeRuntime.EXECUTOR, ModeExecution.PARALLEL_FIXED);
-    }
+      private void executeGenericTest(String inputPath, String outputPath, ModeExecution modeExecution) {
+            Logic logic = LogicFactory.getLogic(ModeRuntime.EXECUTOR, modeExecution);
+            logic.prepare(inputPath, outputPath, YEARS);
+            logic.execute();
+      }
 
-    private void executeGenericTest(String inputPath, String outputPath, ModeRuntime modeRuntime, ModeExecution modeExecution) {
-        logic = LogicFactory.getLogic(modeRuntime, modeExecution);
-        logic.prepare(inputPath, outputPath, YEARS);
-        logic.execute();
-    }
-
-    private long getTime() {
-        return Calendar.getInstance().getTime().getTime();
-    }
+      private long getTime() {
+            return Calendar.getInstance().getTime().getTime();
+      }
 }
