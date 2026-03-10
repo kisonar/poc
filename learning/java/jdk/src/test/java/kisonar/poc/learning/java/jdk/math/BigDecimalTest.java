b91ccdc3b503becd2d3b465e70e90d8f2ff40a54
marcin <marcin.migdal@fortum.com>
2026-03-10 12:23:47 +0100

    cleanup

--- learning/java/jdk/src/test/java/kisonar/poc/learning/java/jdk/math/BigDecimalTest.java
+++ learning/java/jdk/src/test/java/kisonar/poc/learning/java/jdk/math/BigDecimalTest.java
@@ -9,28 +9,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BigDecimalTest {
 
       @Test
-      public void basic() {
+      public void extractParts1dot4523434() {
             BigDecimal bdOne = new BigDecimal("1.4523434");
-            BigDecimal fractionalPart = bdOne.remainder(BigDecimal.ONE); // Result:  0.4523434
-            int fractionalIntValue = fractionalPart.intValue();
-            assertEquals(0, fractionalIntValue);
+
+            BigDecimal fractionalPart = bdOne.remainder(BigDecimal.ONE);
             int bsIntValue = bdOne.intValue();
+
+            assertEquals(0, fractionalPart.intValue());
             assertEquals(1, bsIntValue);
             assertEquals(Double.parseDouble("0.4523434"), fractionalPart.doubleValue());
+      }
 
-            //======================================
+      @Test
+      public void extractParts0dot4523434() {
             BigDecimal bdTwo = new BigDecimal("0.4523434");
-            fractionalPart = bdOne.remainder(BigDecimal.ONE); // Result:  0.4523434
-            fractionalIntValue = fractionalPart.intValue();
-            assertEquals(0, fractionalIntValue);
-            bsIntValue = bdTwo.intValue();
-            assertEquals(0, bsIntValue);
 
+            int fractionalPart = bdTwo.remainder(BigDecimal.ONE).intValue();
+            int bsIntValue = bdTwo.intValue();
+
+            assertEquals(0, fractionalPart);
+            assertEquals(0, bsIntValue);
+      }
+
+      @Test
+      public void extractParts3dot0() {
             BigDecimal bdThree = new BigDecimal("3.0");
-            fractionalPart = bdThree.remainder(BigDecimal.ONE); // Result:  0.4523434
-            fractionalIntValue = fractionalPart.intValue();
-            assertEquals(0, fractionalIntValue);
-            bsIntValue = bdThree.intValue();
+
+            int fractionalPart = bdThree.remainder(BigDecimal.ONE).intValue();
+            int bsIntValue = bdThree.intValue();
+
+            assertEquals(0, fractionalPart);
             assertEquals(3, bsIntValue);
       }
 }
