9076076717d7e499f84723bf0e427fba9bf499da
marcin <marcin.migdal@fortum.com>
2026-03-30 16:18:12 +0200

    Improvements

--- learning/java/my-interview/berlin-clock/src/test/java/kisonar/poc/learning/java/myinterview/berlinclock/logic/display/DisplayDataHoursTest.java
+++ learning/java/my-interview/berlin-clock/src/test/java/kisonar/poc/learning/java/myinterview/berlinclock/logic/display/DisplayDataHoursTest.java
@@ -6,23 +6,23 @@ import org.junit.jupiter.api.Test;
 public class DisplayDataHoursTest {
 
       @Test
-      public void getLampsWhenTopHas2ActivatedAndBottomHas3Activated() {
-            var displayDataHours = new DisplayDataHours(2, 3);
+      public void getLampsWhenTopHas3ActivatedAndBottomHas1Activated() {
+            var displayDataHours = new DisplayDataHours(3, 1);
 
-            Assertions.assertEquals(2, displayDataHours.activatedLampsHoursAtTop());
-            Assertions.assertEquals(2, displayDataHours.getDeactivatedLampsHoursAtTop());
-            Assertions.assertEquals(3, displayDataHours.activatedLampsHoursAtBottom());
-            Assertions.assertEquals(1, displayDataHours.getDeactivatedLampsHoursAtBottom());
+            Assertions.assertEquals(3, displayDataHours.activatedLampsHoursAtTop());
+            Assertions.assertEquals(1, displayDataHours.getDeactivatedLampsHoursAtTop());
+            Assertions.assertEquals(1, displayDataHours.activatedLampsHoursAtBottom());
+            Assertions.assertEquals(3, displayDataHours.getDeactivatedLampsHoursAtBottom());
       }
 
       @Test
-      public void getLampsWhenTopHas4ActivatedAndBottomHas4Activated() {
-            var displayDataHours = new DisplayDataHours(4, 4);
+      public void getLampsWhenTopHas4ActivatedAndBottomHas0Activated() {
+            var displayDataHours = new DisplayDataHours(4, 0);
 
             Assertions.assertEquals(4, displayDataHours.activatedLampsHoursAtTop());
             Assertions.assertEquals(0, displayDataHours.getDeactivatedLampsHoursAtTop());
-            Assertions.assertEquals(4, displayDataHours.activatedLampsHoursAtBottom());
-            Assertions.assertEquals(0, displayDataHours.getDeactivatedLampsHoursAtBottom());
+            Assertions.assertEquals(0, displayDataHours.activatedLampsHoursAtBottom());
+            Assertions.assertEquals(4, displayDataHours.getDeactivatedLampsHoursAtBottom());
       }
 
       @Test
@@ -37,7 +37,9 @@ public class DisplayDataHoursTest {
 
       @Test
       public void shouldThrowExceptionWhenNumberOfLampsIsImproper() {
-            Assertions.assertThrows(IllegalArgumentException.class, () -> new DisplayDataHours(11, 1));
-            Assertions.assertThrows(IllegalArgumentException.class, () -> new DisplayDataHours(1, 7));
+            Assertions.assertThrows(IllegalArgumentException.class, () -> new DisplayDataHours(5, 1));
+            Assertions.assertThrows(IllegalArgumentException.class, () -> new DisplayDataHours(1, 5));
+            Assertions.assertThrows(IllegalArgumentException.class, () -> new DisplayDataHours(-1, 1));
+            Assertions.assertThrows(IllegalArgumentException.class, () -> new DisplayDataHours(1, -1));
       }
 }
