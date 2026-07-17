e4f038afa5e1c647a8ea7a99ace3293652eafa27
MarcinMigdalFortum <marcin.migdal@fortum.com>
2026-07-17 14:00:19 +0200

    repackaging

---
+++ learning/java/jdk/src/test/java/kisonar/poc/learning/java/jdk/concurrent/reactive/ConcurrentTest.java
@@ -0,0 +1,30 @@
+package kisonar.poc.learning.java.jdk.concurrent.reactive;
+
+import org.junit.jupiter.api.Test;
+
+public class ConcurrentTest {
+
+    @Test
+    public void test() {
+
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Hello thread from " + threadName);
+        };
+
+        task.run();
+        System.out.println("Task run from Main");
+
+        Thread thread = new Thread(task);
+        thread.start();
+
+        Thread thread2 = new Thread(task);
+        thread2.start();
+
+        Thread thread3 = new Thread(task);
+        thread3.start();
+
+        System.out.println("Main finished!");
+    }
+
+}
